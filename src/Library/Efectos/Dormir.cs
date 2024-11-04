d3f668a8d1a6e04b7d8461cc8a8c8343a2c560b8
jorgeFilardi-alt <jorgeandresfs01@gmail.com>
2024-11-04 11:23:53 -0300

    Correccion IPokemon efectos

--- src/Library/Efectos/Dormir.cs
+++ src/Library/Efectos/Dormir.cs
@@ -1,6 +1,40 @@
-namespace Library;
+using System;
 
-public class Dormir
+namespace Library
 {
-    public string nombreEfecto {get; set;} = "Dormir";
+    // Clase que aplica el efecto Dormir.
+    public class Dormir : IEfectos
+    {
+        public string nombreEfecto { get; set; } = "Dormir";
+        private int turnosRestante; // Número de turnos que el Pokémon permanecerá dormido.
+
+        // Aplica el efecto de dormir al Pokémon objetivo, estableciendo los turnos de sueño.
+        public void AplicarEfecto(IPokemon objetivo)
+        {
+            objetivo.Estado = "Dormido";
+            
+            // Genera un número aleatorio de turnos entre 1 y 4.
+            Random random = new Random();
+            turnosRestante = random.Next(1, 5);
+        }
+
+        // Verifica si el Pokémon aún está dormido y ajusta el estado en consecuencia.
+        public void VerificarEstado(IPokemon objetivo)
+        {
+            if (turnosRestante > 0)
+            {
+                turnosRestante--; // Reducimos el número de turnos restantes.
+            }
+            else
+            {
+                objetivo.Estado = "Normal"; // El Pokémon despierta y vuelve a la normalidad.
+            }
+        }
+
+        // Método que indica si el Pokémon puede atacar.
+        public bool PuedeAtacar()
+        {
+            return turnosRestante <= 0;
+        }
+    }
 }
--- src/Library/Efectos/Paralizar.cs
+++ src/Library/Efectos/Paralizar.cs
@@ -1,6 +1,23 @@
-namespace Library;
+using System;
 
-public class Paralizar
+namespace Library
 {
-    public string nombreEfecto {get; set;} = "Paralizar";
+    // Clase que aplica el efecto Paralizar.
+    public class Paralizar : IEfectos
+    {
+        public string nombreEfecto { get; set; } = "Paralizar";
+
+        // Aplicar el efecto de parálisis al Pokémon.
+        public void AplicarEfecto(IPokemon objetivo)
+        {
+            objetivo.Estado = "Paralizado";
+        }
+
+        // Determina aleatoriamente si el Pokémon puede atacar.
+        public bool PuedeAtacar()
+        {
+            Random random = new Random();
+            return random.Next(0, 2) == 1; // Retorna true (puede atacar) o false (no puede atacar).
+        }
+    }
 }
