be4808fed9a7fe8927898dee226f284575c178af
MatiasLluis <matias.lluis@gmail.com>
2024-11-06 10:25:31 -0300

    correccion cura total

--- src/Library/TiposItem/CuraTotal.cs
+++ src/Library/TiposItem/CuraTotal.cs
@@ -1,10 +1,6 @@
 namespace Library
 {
-    /// <summary>
-    /// Se pueden usar hasta 2, este item logra que
-    /// se cure a un Pokémon de efectos de ataques especiales, dormido, paralizado, envenenado, o quemado
-    /// </summary>
-    public class CuraTotal:IItem
+    public class CuraTotal : IItem
     {
         public string NombreItem { get; }
         private int usosRestantes;
@@ -13,15 +9,35 @@ namespace Library
         public CuraTotal()
         {
             NombreItem = "Cura Total";
+            usosRestantes = usosMaximos; // Inicializa usosRestantes con el máximo de usos
         }
-        
-        /// <summary>
-        /// Metodo para quitar los efectos
-        /// </summary>
-        /// <returns></returns>
-        public double QuitarEfectos()
+
+        // Método para quitar los efectos
+        public void QuitarEfectos(IPokemon objetivo)
         {
-            return 2;
+            if (usosRestantes > 0)
+            {
+                // Verifica si hay un efecto activo y que no sea "Dormido"
+                if (objetivo.EfectoActivo != null && objetivo.EfectoActivo.nombreEfecto != "Dormido")
+                {
+                    objetivo.EfectoActivo = null;
+                    objetivo.Estado = "Normal";
+                    Console.WriteLine($"{objetivo.Nombre} ya no está bajo ningún efecto");
+                    usosRestantes--;
+                }
+                else if (objetivo.EfectoActivo?.nombreEfecto == "Dormido")
+                {
+                    Console.WriteLine($"{objetivo.Nombre} está dormido y no puede recibir Cura Total.");
+                }
+                else
+                {
+                    Console.WriteLine($"{objetivo.Nombre} no tiene efectos activos.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se pueden usar más Cura Total en esta batalla.");
+            }
         }
     }
 }
